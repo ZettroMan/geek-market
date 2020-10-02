26e2db35aff6fe980ef537f0f53c3bca55ccb661
Roman Russkikh <r-ru@yandex.ru>
2020-10-02 21:38:20 +0300

    Initial commit

---
+++ src/main/java/com/geekbrains/geek/market/GeekMarketApplication.java
@@ -0,0 +1,17 @@
+package com.geekbrains.geek.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekMarketApplication {
+	// Домашнее задание:
+	// 1. На странице корзины делаем кнопку оформить заказ, нас перебрасывает на страницу,
+	// где показываются товары из корзины, без возможности их менять
+	// 2. На странице оформления заказа указывается имя клиента, его номер телефона, и адрес
+	// доставки, по нажатию на кнопку оформить, заказ должен быть сохранен в базу данных
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekMarketApplication.class, args);
+	}
+}
