bbe4b48eceedc0baf30d9d777aca8a2172f54516
Roman Russkikh <r-ru@yandex.ru>
2020-10-06 15:08:50 +0300

    Lesson 7 REST

--- src/main/java/com/geekbrains/geek/market/controllers/RestProductController.java
+++ src/main/java/com/geekbrains/geek/market/controllers/RestProductController.java
@@ -2,11 +2,14 @@ package com.geekbrains.geek.market.controllers;
 
 import com.geekbrains.geek.market.entities.Product;
 import com.geekbrains.geek.market.services.ProductService;
+import com.geekbrains.geek.market.utils.ProductFilter;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/products")
@@ -15,8 +18,14 @@ public class RestProductController {
     private ProductService productService;
 
     @GetMapping // /api/v1/products
-    public List<Product> getAllProducts() {
-        return productService.findAll(Specification.where(null), 0, 10).getContent();
+    public Page<Product> getProducts(@RequestParam(defaultValue = "1", name = "p") Integer page,
+                                     @RequestParam(defaultValue = "5", name = "s") Integer size,
+                                     @RequestParam Map<String, String> params) {
+        if (page < 1) {
+            page = 1;
+        }
+        ProductFilter productFilter = new ProductFilter(params);
+        return productService.findAll(productFilter.getSpec(), page - 1, size);
     }
 
     @GetMapping("/{id}")
